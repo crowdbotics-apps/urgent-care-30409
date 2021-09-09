f4d22dd995091e3279766ee1331315ae9491d003
Crowdbotics <team@crowdbotics.com>
2021-09-09 05:57:31 +0000

    Update models

--- backend/urgent_care_30409/wsgi.py
+++ backend/urgent_care_30409/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'urgent_care_30409.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "urgent_care_30409.settings")
 
 application = get_wsgi_application()
