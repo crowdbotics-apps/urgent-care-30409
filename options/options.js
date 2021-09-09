4bbcb33b8e870de1465e54a1e2ea354a0720b6fb
Crowdbotics <team@crowdbotics.com>
2021-09-09 05:50:24 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "urgent_care_30409Identifier",
+  url: "https://urgent_care_30409Identifier.botics.co",
+  api: "https://urgent_care_30409Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
